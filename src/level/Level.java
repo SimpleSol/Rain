51b76e6b8071b4a102be34a8c4706251b65e8fe7
SimpleSol <gworkmaill@gmail.com>
2016-06-02 15:41:14 +0300

    Episode 24

---
+++ src/level/Level.java
@@ -0,0 +1,45 @@
+package level;
+
+import graphics.Screen;
+
+/**
+ * Created by Leon on 02.06.2016.
+ */
+public class Level {
+
+    private int width;
+    private int height;
+    private int[] tiles;
+
+    public Level(int width, int height) {
+        this.width = width;
+        this.height = height;
+        tiles = new int[width * height];
+        generateLevel();
+    }
+
+    public Level(String path) {
+        loadLevel(path);
+    }
+
+    private void generateLevel() {
+
+    }
+
+    private void loadLevel(String path) {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void render(int xScroll, int yScroll, Screen screen) {
+
+    }
+
+    private void time() {
+
+    }
+
+}
